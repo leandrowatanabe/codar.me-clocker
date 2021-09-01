1247319514eab22ff79cccbad69e36506fbe6100
Leandro Watanabe - DESKTOP <setsuo.leandro@gmail.com>
2021-09-01 14:54:20 -0300

    firebase admin

--- config/firebase/inidex.js
+++
@@ -1,4 +0,0 @@
-
-import firebaseClient from "./client";
-
-export { firebaseClient }
--- next.config.js
+++ next.config.js
@@ -17,6 +17,12 @@ module.exports = {
     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    project_id: process.env.PROJECT_ID ,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY,
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID ,
+    client_x509_cert_url: process.env.CLIENT_CERT 
   }
 };
